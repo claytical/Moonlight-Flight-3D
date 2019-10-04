f38bc470aa0ee2f208c0dea1acf6021fb8f03f8b
Clay Ewing <c.ewing@miami.edu>
2019-10-04 15:40:35 -0400

    Initial commit

---
+++ Assets/EmittedObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmittedObject : MonoBehaviour
+{
+    public float lifeTime;
+    private float startTime;
+    public float force;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        lifeTime = lifeTime + startTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * force;
+        if(lifeTime < Time.time)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
