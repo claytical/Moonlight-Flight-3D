41bd2422ebf09c9cce46fa4b402b8f0da0fd41dc
Clay Ewing <c.ewing@miami.edu>
2019-10-07 13:22:34 -0400

    Added SteamVR
    Wind Blower generates rigidbodies to push objects

---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Emitter : MonoBehaviour
+{
+    
+    public GameObject objectToEmit;
+    public float timeBetweenEmits;
+    public bool emitting;
+    private float nextEmitTime;
+    public GameObject emitterContainer;
+    
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        nextEmitTime = Time.time + timeBetweenEmits;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        emitting = SteamVR_Actions._default.GrabPinch.state;
+        if (emitting)
+        {
+            if(nextEmitTime <= Time.time)
+            {
+                GameObject go = Instantiate(objectToEmit, emitterContainer.transform);
+                //                go.GetComponent<EmittedObject>().;
+                go.transform.position = transform.position;
+                go.transform.rotation = transform.rotation;
+                go.GetComponent<Rigidbody>().velocity = transform.forward * 5f;
+                go.GetComponent<EmittedObject>().force = SteamVR_Actions._default.BlowAir.GetAxis(SteamVR_Input_Sources.RightHand) * 50f;
+                //between 0 and 1, currently .2
+
+                timeBetweenEmits = 1 - SteamVR_Actions._default.BlowAir.GetAxis(SteamVR_Input_Sources.RightHand);
+                Debug.Log("Time Between Emits: " + timeBetweenEmits);
+
+                nextEmitTime = Time.time + timeBetweenEmits;
+            }
+        }
+    }
+    public void TriggerToggle()
+    {
+        emitting = !emitting;
+    }
+}
